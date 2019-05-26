a4f6887620f9fe9ecf79e906d1e4431d065d95a1
Alexander <atenefyr@gmail.com>
2019-05-26 14:59:33 -0500

    Update NoFishTimer.cs

--- NoFishTimer.cs
+++ NoFishTimer.cs
@@ -3,15 +3,15 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace NoFishTimer {
-	class NoFishTimer : Mod {
-		public NoFishTimer() {}
-	}
-	
-    	public class thisWorld : ModWorld {
-        	public override void PreUpdate() {
-            		if (Main.netMode != 1 && Main.anglerWhoFinishedToday.Count > 0) {
-				Main.AnglerQuestSwap();
-            		}
-        	}
-    	}
+    class NoFishTimer : Mod {
+        public NoFishTimer() {}
+    }
+    
+    public class NoFishWorld : ModWorld {
+        public override void PreUpdate() {
+            if (Main.netMode != 1 && Main.anglerWhoFinishedToday.Count > 0) {
+                Main.AnglerQuestSwap();
+            }
+        }
+    }
 }
